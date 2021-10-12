a0574c66f8baf97031c416a2948f12095777aaa8
KhanhNguyen1102 <sukhanh12@gmail.com>
2021-10-12 21:18:03 +0700

    update

--- src/DemoBook/Main.java
+++ src/DemoBook/Main.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Book[] listBook = new Book[10];
+        Book[] listBook = createListBook();
         input(listBook);
         output(listBook);
         System.out.println("Tổng tiền các cuốn sách là :" + sumPrice(listBook));
         findBook(listBook);
     }
-
+//    Hàm tạo mảng Book
+    public  static Book[] createListBook(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số lượng phần tử trong mảng Book :");
+        int size = scanner.nextInt();
+        return new Book[size];
+    }
+//    Hàm điền phần tử cho mảng Book
     public static void input(Book[] listBook) {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < listBook.length; i++) {
             System.out.println("Nhập vào thông tin");
             String name = scanner.nextLine();
             System.out.println("Nhập vào thể loại ");
@@ -29,21 +36,21 @@ public class Main {
             listBook[i] = book;
         }
     }
-
+//    Hàm in mảng Book
     public static void output(Book[] listBook) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < listBook.length; i++) {
             System.out.println(listBook[i]);
         }
     }
-
+//    Hàm tính tổng tiền 1 loại sách
     public static double sumPrice(Book[] listBook) {
         double sumPrice = 0;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < listBook.length; i++) {
             sumPrice += listBook[i].getPrice() * listBook[i].getAmount();
         }
         return sumPrice;
     }
-
+//    Hàm tìm 1 quyển sách có trong mảng ko , in ra thông tin
     public static void findBook(Book[] listBook) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập vào tên quyển sách muốn tìm ");
